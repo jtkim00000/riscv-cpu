b6ff181c4521aa1cdf5d126ba1db7d434eddd9c1
jtkim00000 <jessetkim00000@gmail.com>
2026-06-05 04:00:42 -0500

    changed memory data to byte addressability, need to test

--- src/DataMemory.v
+++ src/DataMemory.v
@@ -2,21 +2,28 @@ module data_memory (
     input clk,
     input we, //write enable
     input re, //read enable
+    input num_bytes,
     input [31:0] addr,
     input [31:0] wdata,
     output reg [31:0] rdata
 );
     // Smaller size for simulation
-    reg [31:0] data_memory [0:255];
+    reg [7:0] data_memory [0:255];
 
     always @(*) begin
-        if(re)
-            rdata = data_memory[addr[7:0]];
+        if(re) begin
+            for(integer i = 0; i < num_bytes; i++) begin
+                rdata[i*8 +: 8] = data_memory[addr[7:0] + i];
+            end
+        end
     end
 
     always @(posedge clk) begin
-        if(we)
-            data_memory[addr[7:0]] <= wdata;
+        if(we) begin
+            for(integer i = 0; i < num_bytes; i++) begin
+                data_memory[addr[7:0] + i] = wdata[i*8 +: 8];
+            end
+        end
     end
 
 endmodule
