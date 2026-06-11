f76357f28de69f0be89a17c3124af770905b7261
jtkim00000 <jessetkim00000@gmail.com>
2026-06-11 00:29:31 -0500

    passed register-register operations

--- src/InstructionMemory.v
+++ src/InstructionMemory.v
@@ -4,14 +4,14 @@ module instruction_memory (
 );
 
     // Smaller size for simulation
-    reg [7:0] instruction_memory [0:255];
+    reg [7:0] instruction_memory [0:1023];
 
     // Load program
     initial begin
-        $readmemh("prog/cpu_test_lui.hex", instruction_memory, 8'd0);
+        $readmemh("prog/cpu_test_reg_op.hex", instruction_memory, 8'd0);
     end
 
     //using 32-bit word size, but using the higher bits of pc
-    assign instr = {instruction_memory[raddr[7:0]], instruction_memory[raddr[7:0]+1], instruction_memory[raddr[7:0]+2], instruction_memory[raddr[7:0]+3]};
+    assign instr = {instruction_memory[raddr[9:0]], instruction_memory[raddr[9:0]+1], instruction_memory[raddr[9:0]+2], instruction_memory[raddr[7:0]+3]};
 
 endmodule
