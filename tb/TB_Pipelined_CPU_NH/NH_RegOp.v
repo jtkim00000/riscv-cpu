deecd8beccf548550bd11773a3f38d34f12a43ed
jtkim00000 <jessetkim00000@gmail.com>
2026-06-18 01:59:08 -0500

    Pipelined CPU LUI tests passed, no hazards

---
+++ tb/TB_Pipelined_CPU_NH/NH_RegOp.v
@@ -0,0 +1,75 @@
+`timescale 1ns/1ps
+
+module NH_RegOp;
+
+    reg clk;
+    reg reset;
+
+    reg [31:0] expected [0:31];
+    integer i;
+    integer errors;
+
+    initial begin
+        expected[0]  = 32'h00000000;
+        expected[1]  = 32'h00001001;
+        expected[2]  = 32'h00002000;
+        expected[3]  = 32'h00003000;
+        expected[4]  = 32'h00004000;
+        expected[5]  = 32'h00005000;
+        expected[6]  = 32'h00006000;
+        expected[7]  = 32'h00007000;
+        expected[8]  = 32'h00008000;
+        expected[9]  = 32'h00009000;
+
+        expected[10] = 32'h00001001;
+        expected[11] = 32'hFFFFEFFF;
+        expected[12] = 32'h00001001;
+        expected[14] = 32'h00009001;
+        expected[15] = 32'h00001000;
+        expected[16] = 32'h00004000;
+        expected[17] = 32'h00001000;
+        expected[18] = 32'h00001000;
+        expected[19] = 32'h00000001;
+        expected[20] = 32'h00000001;
+    end
+
+    p_cpu test_cpu (.clk(clk), .reset(reset));
+
+    always #5 clk = ~clk;
+
+    initial begin
+        clk = 1'b0;
+
+        $dumpfile("sim/nh_reg_op.vcd");
+        $dumpvars(1, NH_RegOp);
+
+        reset = 1'b1;
+
+        @(posedge clk); #1;
+
+        reset = 1'b0;
+
+        repeat(100) @(posedge clk);
+
+        errors = 0;
+
+        for (i = 0; i < 21; i = i + 1) begin
+            if (test_cpu.register_file.register[i] !== expected[i]) begin
+                errors = errors + 1;
+                $display("FAIL AT R%0d: expected %08h, got %08h",
+                        i,
+                        expected[i],
+                        test_cpu.register_file.register[i]);
+            end
+        end
+
+        if (errors == 0)
+            $display("PASS: No Hazards Pipelined Register-Register Operations test");
+        else
+            $display("FAIL: %0d register mismatches", errors);
+
+        $finish;
+
+    end
+
+endmodule
